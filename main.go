814cc7a1cd638513ecef22c95398521aacb779ec
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-05 12:37:45 +0700

    ubah jadi config

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(Config)
+	app.Use(cors.New(Cors))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(IPPort)
 }
